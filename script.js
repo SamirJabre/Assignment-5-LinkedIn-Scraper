4278d247fc0f52e1d7a4208808738bdde3291289
Samir Jabre <samirjabre8@gmail.com>
2024-07-06 02:06:59 +0300

    Job Location Added

--- script.js
+++ script.js
@@ -7,14 +7,37 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.linkedin.com/jobs/search/?currentJobId=3947001319&origin=JOBS_HOME_JYMBII');
 
+  
+
+  // await page.waitForSelector('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
+
+  // // Click on the link
+  // await page.click('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
+
+  // // Wait for navigation to complete (if needed)
+  // await page.waitForNavigation();
+
+  // console.log('Link clicked and navigation completed.');
+
+
+
+
+  // await page.locator('.base-card__full-link').click();
+
   const jobHandles=await page.$$('.jobs-search__results-list > li');
+  // const jobHandles=await page.$$('.jobs-search__results-list > li');
+
+
 
   for(const jobhandle of jobHandles){
+    
+    // await page.locator('.base-card__full-link').click();
     const title = await page.evaluate(el  => el.querySelector('.base-search-card__title').textContent,jobhandle);
     const companyName = await page.evaluate(el  => el.querySelector('.base-search-card__subtitle').textContent,jobhandle);
-    let i;
-    console.log(`${i}- Job Title : ${title} , Company Name : ${companyName}`);
-    i++
+    const jobLocation = await page.evaluate(el  => el.querySelector('.job-search-card__location').textContent,jobhandle);
+    
+    console.log(`- Job Title : ${title} , Company Name : ${companyName} , Job Location : ${jobLocation}`);
+
   }
 
 
