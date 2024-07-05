3fb07daf9e71e2ee6263247bbf16d130867a4682
Samir Jabre <samirjabre8@gmail.com>
2024-07-06 02:23:11 +0300

    Finished Result of simple scraper

--- script.js
+++ script.js
@@ -7,38 +7,78 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.linkedin.com/jobs/search/?currentJobId=3947001319&origin=JOBS_HOME_JYMBII');
 
+  const jobHandles=await page.$$('.jobs-search__results-list > li');
+  for(const jobhandle of jobHandles){
+    const title = await page.evaluate(el  => el.querySelector('.base-search-card__title').textContent,jobhandle);
+    const companyName = await page.evaluate(el  => el.querySelector('.base-search-card__subtitle').textContent,jobhandle);
+    const jobLocation = await page.evaluate(el  => el.querySelector('.job-search-card__location').textContent,jobhandle);
+    
+    console.log(`- Job Title : ${title} , Company Name : ${companyName} , Job Location : ${jobLocation}`);
+  }
+
+})();
+
+
+
+// ALL OF COMMENTED CODE DOWN THERE ARE EXPERIMENTAL :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // for(const jobhandle of jobHandles){
   
+  //   const title = await page.evaluate(el  => el.querySelector('.base-search-card__title').textContent,jobhandle);
+  //   const companyName = await page.evaluate(el  => el.querySelector('.base-search-card__subtitle').textContent,jobhandle);
+  //   const jobLocation = await page.evaluate(el  => el.querySelector('.job-search-card__location').textContent,jobhandle);
+    
+  //   console.log(`- Job Title : ${title} , Company Name : ${companyName} , Job Location : ${jobLocation}`);
 
-  // await page.waitForSelector('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
+  // }
 
-  // // Click on the link
-  // await page.click('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
-
-  // // Wait for navigation to complete (if needed)
-  // await page.waitForNavigation();
-
-  // console.log('Link clicked and navigation completed.');
+  
 
 
 
 
   // await page.locator('.base-card__full-link').click();
 
-  const jobHandles=await page.$$('.jobs-search__results-list > li');
+  // const element=await page.$$('.top-card-layout__card relative');
+  //   const title = await page.evaluate(el => el.querySelector('.top-card-layout__title').textContent, element);
+  //   console.log('Text Content:', title);
+
+  // if (element) {
+  //   // Example: Get the text content
+  //   const title = await page.evaluate(el => el.querySelector('.top-card-layout__title').textContent, element);
+  //   console.log('Text Content:', title);
+  // }
+
+
+  // const jobHandles=await page.$$('.jobs-search__results-list > li');
   // const jobHandles=await page.$$('.jobs-search__results-list > li');
 
 
 
-  for(const jobhandle of jobHandles){
-    
-    // await page.locator('.base-card__full-link').click();
-    const title = await page.evaluate(el  => el.querySelector('.base-search-card__title').textContent,jobhandle);
-    const companyName = await page.evaluate(el  => el.querySelector('.base-search-card__subtitle').textContent,jobhandle);
-    const jobLocation = await page.evaluate(el  => el.querySelector('.job-search-card__location').textContent,jobhandle);
-    
-    console.log(`- Job Title : ${title} , Company Name : ${companyName} , Job Location : ${jobLocation}`);
-
-  }
+  
 
 
 
@@ -47,12 +87,20 @@ const puppeteer = require('puppeteer');
   // await page.waitForSelector('span');
   // const title = await page.$eval('span', element => element.textContent);
   // console.log('Extracted Text:', title);
-})();
 
 
 
 
 
+// await page.waitForSelector('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
+
+  // // Click on the link
+  // await page.click('.jobs-search__results-list > li > .base-card__full-link'); // Adjust the selector as needed
+
+  // // Wait for navigation to complete (if needed)
+  // await page.waitForNavigation();
+
+  // console.log('Link clicked and navigation completed.');
 
 
 
